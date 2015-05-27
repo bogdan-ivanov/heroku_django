db38f5f2c60b38330f4f76ec5ba0425fab5f8205
George-Bogdan Ivanov <ivanov.george.bogdan@gmail.com>
2015-05-27 13:44:14 -0700

    Creating post model and admin

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,6 @@ class Post(models.Model):
     title = models.CharField(max_length=500)
     body = models.TextField()
     author = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return self.title
